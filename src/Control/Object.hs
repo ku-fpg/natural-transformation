79e9fc3c21b58d5819a4c7fa12e293847a9b4758
Andy Gill <andygill@ku.edu>
2015-08-12 22:50:09 -0500

    Adding Object, bumping v#

---
+++ src/Control/Object.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE Safe #-}
+
+{-|
+Module:      Control.Object
+Copyright:   (C) 2015 The University of Kansas
+License:     BSD-style (see the file LICENSE)
+Maintainer:  Andy Gill
+Stability:   Experimental
+
+An Object type.
+-}
+
+module Control.Object (Object(..)) where
+        
+import Control.Natural
+import Control.Transformation
+
+-- | An 'Object' is a natural transformation from a given 'Functor' 'f', to 'IO'.
+newtype Object f = Object (f :~> IO)
+
+instance Transformation f IO (Object f) where
+    Object f # g = f $$ g
+
